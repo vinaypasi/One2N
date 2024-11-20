7b5de6964eb1a445bb4d4841f0996def4d2d043c
Vinay Pasi <vinay.pasi@datagrokr.com>
2024-11-21 03:12:22 +0530

    UPDATE/Updated user data for ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,20 +74,6 @@ resource "aws_security_group" "http_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -5,7 +5,7 @@ sudo yum install -y python3 git pip
 
 
 # Clone the repository
-# git clone https://github.com/username/repository.git /home/ec2-user/app
+wget https://raw.githubusercontent.com/vinaypasi/One2N/main/app.py -O ~/app.py
 
 # Install Python dependencies
 sudo pip3 install flask boto3
@@ -16,5 +16,4 @@ sudo openssl genpkey -algorithm RSA -out server-key.pem
 sudo openssl req -new -key server-key.pem -out server-csr.pem -subj "/C=US/ST=State/L=City/O=Company/OU=Dev/CN=localhost"
 sudo openssl x509 -req -in server-csr.pem -signkey server-key.pem -out server-cert.pem
 # Run the app
-# cd /home/ec2-user/app
-# nohup python3 app.py > app.log 2>&1 &
+nohup python3 ~/app.py > app.log 2>&1 &
